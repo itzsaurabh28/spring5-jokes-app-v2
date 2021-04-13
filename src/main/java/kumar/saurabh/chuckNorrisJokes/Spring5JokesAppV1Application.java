333bc900b20b4aa6a8ff22963ed10d11da710a0f
itzsaurabh28 <kumarsaurabh28@outlook.com>
2021-04-13 13:22:11 +0530

    First commit

---
+++ src/main/java/kumar/saurabh/chuckNorrisJokes/Spring5JokesAppV1Application.java
@@ -0,0 +1,13 @@
+package kumar.saurabh.chuckNorrisJokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5JokesAppV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/kumar/saurabh/chuckNorrisJokes/Spring5JokesAppV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package kumar.saurabh.chuckNorrisJokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
